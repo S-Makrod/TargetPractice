2fa3dad83dc29ecf9b1a8236fb6d1d94381332bd
Andrei Fedchenko <afedchenko@guidewire.com>
2024-07-25 15:17:37 -0400

    final perfect version

--- scripts.js
+++ scripts.js
@@ -9,7 +9,24 @@ let targetMovedAt;
 let scoreInput;
 let avgReactionInput;
 let bestReactionInput;
+let currentReactionInput;
 let history = [];
+
+let audio;
+
+const nickImgs = [
+  "assets/pictures/NCMeme1.png",
+  "assets/pictures/NCMeme2.png",
+  "assets/pictures/NCMeme3.png",
+  "assets/pictures/NCNationalTreasure.png",
+];
+const nickAudio = [
+  "assets/sounds/HOWD_IT_GET_BURNED.mp3",
+  "assets/sounds/IM_GON_SAVE_THE_DAY.mp3",
+  "assets/sounds/PISSED_BLOOD.mp3",
+  "assets/sounds/PUT_THE_BUNNY_BACK_IN_BOX.mp3",
+  "assets/sounds/rocky_insides.mp3",
+];
 
 function hide(element) {
   element.classList.remove("display");
@@ -28,8 +45,8 @@ function cumulative(accumulator, i) {
 }
 
 function moveTarget() {
-  const x = Math.floor(Math.random() * (width - 50) + 50);
-  const y = Math.floor(Math.random() * (height - 50) + 50);
+  const x = Math.floor(Math.random() * (width - 150) + 150);
+  const y = Math.floor(Math.random() * (height - 150) + 150);
 
   target.style.left = `${x}px`;
   target.style.top = `${y}px`;
@@ -38,6 +55,8 @@ function moveTarget() {
 }
 
 function targetOnClick() {
+  target.src = nickImgs[Math.floor(Math.random() * 4)];
+
   let localScore = 100;
 
   const localReaction = new Date() - targetMovedAt;
@@ -50,9 +69,14 @@ function targetOnClick() {
 
   scoreInput.value = Number(scoreInput.value) + localScore;
   const average = cumulative(0, history.length - 1) / history.length / 1000;
-  avgReactionInput.value =
-    Math.round(average * 1000) / 1000 + "s";
+  avgReactionInput.value = Math.round(average * 1000) / 1000 + "s";
   bestReactionInput.value = Math.min(...history) / 1000 + "s";
+  currentReactionInput.value = localReaction / 1000 + "s";
+
+  if (audio) audio.pause();
+  audio = new Audio(nickAudio[Math.floor(Math.random() * 5)]);
+  audio.volume = 1;
+  audio.play();
 
   moveTarget();
 }
@@ -61,8 +85,14 @@ function startGame() {
   hide(button);
   display(target);
 
-  width = document.getElementById("game-area").offsetWidth - 50;
-  height = document.getElementById("game-area").offsetHeight - 50;
+  width = document.getElementById("game-area").offsetWidth - 150;
+  height = document.getElementById("game-area").offsetHeight - 150;
+
+  const backgroundMusic = new Audio("assets/sounds/pedro-song.mp3");
+
+  backgroundMusic.volume = 0.15;
+  backgroundMusic.loop = true;
+  backgroundMusic.play();
 
   moveTarget();
 }
@@ -74,6 +104,16 @@ window.onload = () => {
   scoreInput = document.getElementById("score-input");
   avgReactionInput = document.getElementById("avg-reaction");
   bestReactionInput = document.getElementById("best-reaction");
+  currentReactionInput = document.getElementById("current-reaction");
 
   target.onclick = targetOnClick;
+
+  gameArea.onclick = () => {
+    const shotAudio = new Audio(
+      "assets/sounds/m4a1_single-kibblesbob-8540445.mp3"
+    );
+
+    shotAudio.volume = 0.3;
+    shotAudio.play();
+  };
 };
